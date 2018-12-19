13bc34227fbd34d78f989efd87d4aeb3bc633a86
Michael Warneke <michael.warneke@outlook.com.au>
2018-12-19 11:38:12 +0800

    Dash:0.0.1

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dash', loadChildren: '../dash/dash.module#DashModule' },
+  { path: '', redirectTo: '/dash', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
